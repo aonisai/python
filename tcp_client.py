892ea6a83b39b053a063da2cfc4ee49c30724405
aonisai <seiunryou0602@gmail.com>
2016-10-28 01:05:28 +0900

    change download.py & upload.py

--- tcp_client.py
+++ tcp_client.py
@@ -2,10 +2,10 @@
 
 import socket
 
-#target_host = "127.0.0.1"
-target_host = "www.google.com"
-target_port = 80
-#target_port = 4000
+target_host = "127.0.0.1"
+#target_host = "www.google.com"
+#target_port = 80
+target_port = 4000
 
 # make socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,8 +14,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 # send data
-client.send("GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
-#client.send("Hello!")
+#client.send("GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+client.send("Hello!")
 
 # receive data
 response = client.recv(4096)
--- upload.py
+++ upload.py
@@ -1,6 +1,4 @@
-"""Upload the contents of your Downloads folder to Dropbox.
-This is an example app for API v2.
-"""
+# upload file to dropbox
 
 from __future__ import print_function
 
@@ -17,18 +15,51 @@ import unicodedata
 #    input = raw_input
 
 import dropbox
-from dropbox.files import FileMetadata, FolderMetadata
+#from dropbox.files import FileMetadata, FolderMetadata
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError
 
 # OAuth2 access token.  TODO: login etc.
 #dbx = dropbox.Dropbox('VayI-kUhGUAAAAAAAAAADah23S3nYG3CK0Q_lH59VKk0_c_tj5Uu2lJzax_EEvar')
 
-def main():
+parser = argparse.ArgumentParser(description='upload a file to Dropbox')
+parser.add_argument('--file', '-f', help='file path')
+parser.add_argument('--dbpath', '-d', help='dropbox path')
+
+# upload a file
+if __name__ == '__main__':
+    #main()
 
     dbx = dropbox.Dropbox('VayI-kUhGUAAAAAAAAAADah23S3nYG3CK0Q_lH59VKk0_c_tj5Uu2lJzax_EEvar')
-#    dbx = dropbox.Dropbox(args.token)
+    #    dbx = dropbox.Dropbox(args.token)
 
+    args = parser.parse_args()
+    if not args.file:
+        print("Please specify the file" + "Usage: upload.py -f file/path -d dropbox/path")
+        exit(1)
+    f = args.file
+    has_dbpath = hasattr(args, 'dbpath')
+    if not has_dbpath:
+        print("Oh NO!")
+        sys.exit(1)
+    if not args.dbpath:
+        print("1")
+        dbpath = '/' + f
+    else:
+        print("YES")
+        dbpath = args.dbpath
 
-
-if __name__ == '__main__':
-    main()
-
+    with open(f, 'rb') as f:
+        #
+        try:
+            dbx.files_upload(f, dbpath, mode=WriteMode('overwrite'))
+        except ApiError as error:
+            if (error.error.is_path() and error.error.get_path().error.is_insufficient_space()):
+                print("error")
+                #sys.exit("Cannot back up; insufficient space.")
+            elif error.user_message_text:
+                print(error.user_message_text)
+                sys.exit()
+            else:
+                print(error)
+                sys.exit()
