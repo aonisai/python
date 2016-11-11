483efe0cfd2991816d1cb9520582fd7f88500ab1
aonisai <seiunryou0602@gmail.com>
2016-11-12 01:06:52 +0900

    add Box/start.py

--- Box/start.py
+++ Box/start.py
@@ -1,23 +1,24 @@
 from boxsdk import OAuth2
 from boxsdk import Client
 
-"""
-oauth = OAuth2(
-    client_id='YOUR_CLIENT_ID',
-    client_secret='YOUR_CLIENT_SECRET',
-    store_tokens=your_store_tokens_callback_method,
-)
-
-auth_url, csrf_token = oauth.get_authorization_url('http://YOUR_REDIRECT_URL')
-"""
-
 oauth = OAuth2(
   client_id='bde3fxtg8ysjbrtdhlflftc1u9brsnbl',
   client_secret='jxfAFzhTdPA2DXBAIXyz4fIPl4OjzwAR',
-  access_token='uvKd4XXjzv5aQLiK8THZAExkfEoCIGHt',
+  access_token='8ld6OVN2ktWgsPBHqLKcKqoyY2copXvW',
 )
 client = Client(oauth)
-root_folder = client.folder(folder_id='0')
-shared_folder = root_folder.create_subfolder('shared_folder')
-uploaded_file = shared_folder.upload('test.txt')
-shared_link = shared_folder.get_shared_link()
+#root_folder = client.folder(folder_id='0')
+#shared_folder = root_folder.create_subfolder('shared_folder')
+#uploaded_file = shared_folder.upload('test.txt')
+#shared_link = shared_folder.get_shared_link()
+
+me = client.user(user_id='me').get()
+print('user_login:' + me['login'])
+
+root_folder = client.folder(folder_id='0').get()
+print('folder owner:' + root_folder.owned_by['login'])
+print('folder name:' + root_folder['name'])
+
+items = client.folder(folder_id='0').get_items(limit=100, offset=0)
+
+client.file(file_id='SOME_FILE_ID').get()['name']
