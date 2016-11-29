486b762a2d5f968a1d88cd4f5e6d3d4da4103859
aonisai <seiunryou0602@gmail.com>
2016-11-30 01:02:03 +0900

    modify

--- Box/upload.py
+++ Box/upload.py
@@ -5,7 +5,7 @@ import argparse
 oauth = OAuth2(
   client_id='bde3fxtg8ysjbrtdhlflftc1u9brsnbl',
   client_secret='jxfAFzhTdPA2DXBAIXyz4fIPl4OjzwAR',
-  access_token='2SR1ui9IorZeHiVK9GzQgzXmievIyD3R',
+  access_token='MiQd8HyjkN0jdvIClJ8o8fJFlRMT9jMJ',
 )
 client = Client(oauth)
 
