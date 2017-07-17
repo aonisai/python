d3299ee462da6136cfbcfddc91354061de657dac
aonisai <seiunryou0602@gmail.com>
2017-07-17 15:49:22 +0900

    modify cve.py

--- cookbook/cve.py
+++ cookbook/cve.py
@@ -1,12 +1,28 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-def scanner(fileobject, linehandler):
-    for line in fileobject:
-        linehandler(line)
+import def_scanner
+import re
 
-import scanner
+
 def firstword(line):
-    print line.split()[0]
-file = open('/home/enigma/temp/cve_note')
-scanner(file, firstword)
+    # print line.split()[0]
+    print line
+
+
+def find_cve(line):
+    # print re.findall('cve-....-....', line, re.IGNORECASE)
+    string_list = re.findall('cve-....-....', line, re.IGNORECASE)
+    # print string_list
+    return string_list
+
+
+def remove_room(line):
+    removed_list = filter(lambda s: s != '', line)
+    print removed_list
+
+# file = open('/home/enigma/temp/cve_note')
+file = open('/home/enigma/temp/test')
+# def_scanner.scanner(file, find_cve)
+def_scanner.scanner(file, remove_room)
+
